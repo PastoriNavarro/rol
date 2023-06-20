3f52995faf3984d9bd82d8b069260dc0f33db36b
PastoriNavarro <115813986+PastoriNavarro@users.noreply.github.com>
2023-06-20 12:12:13 +0200

    Add files via upload

---
+++ java/Bestia.java
@@ -0,0 +1,52 @@
+package com.Pastori.Rol.java;
+
+//clase que hereda de personajes y donde se seguiran las especificaciones pedidas solo para los de tipo com.Pastori.Rol.java.Bestia.
+public class Bestia extends Personaje {
+
+    private Tipo tipo;
+
+    enum Tipo{
+        ORCOS,
+        TRASGOS;
+    }
+
+    public Bestia(String nombre, int puntosVida, int resistencia, Tipo tipo) {
+        super(nombre, puntosVida, resistencia);
+        this.tipo = tipo;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return getNombre() + " (Vida = " + getPuntosVida() + " Armadura = " + getResistencia() + ")";
+    }
+
+    //Lanzar 1 dado para el ataque de las bestias sacando números aleatorios del 0 a 90
+    public int lanzarDados() {
+        int dado1 = (int) (Math.random() * 90) * 1;
+
+        return dado1;
+    }
+
+    //ataque de las bestias restando el resultado del lanzamiento de dados a la resistencia del personaje
+    public void ataque(Heroe heroe) {
+        int ataque = lanzarDados();
+        int resistencia = heroe.getResistencia();
+        int herida = ataque - resistencia;
+
+        //Si el daño(herida) realizado es menor que 0 la bestia no podrá atacar porque la resistencia del defensor es mayor que la potencia de ataque
+            if (herida <= 0){
+                System.out.println("El ataque no se puede producir porque la resistencia del defensor es mayor que la potencia de ataque");;
+            }
+
+        System.out.println("Potencia de ataque : " + ataque + "\n" + " puntos " + "resitencia " + resistencia + "\n" + "herida: " + herida );
+
+    }
+}
---
+++ java/Turno.java
@@ -0,0 +1,88 @@
+package com.Pastori.Rol.java;
+
+import com.Pastori.Rol.java.Bestia;
+import com.Pastori.Rol.java.Heroe;
+
+import java.util.List;
+
+public class Turno {
+
+    //declaración de listas de heroes y bestias sobre las que vamos a trabajar creando turnos para luchar
+    private final List<Heroe> heroes;
+    private final List<Bestia> bestias;
+
+    public Turno(List<Heroe> heroes, List<Bestia> bestias) {
+        this.heroes = heroes;
+        this.bestias = bestias;
+    }
+
+    //ataque de heroes a bestias separados por los distintos tipos de heroes que se piden y sus distintas formas de atacar
+    public void ataque(Bestia bestia, Heroe heroe) {
+        int ataqueH = heroe.lazarDados();
+        int resistenciaB = bestia.getResistencia();
+        int heridaH = ataqueH - resistenciaB;
+
+        switch (heroe.getTipo()){
+            case ELFOS:
+                if(bestia.getTipo() == Bestia.Tipo.ORCOS){
+                    heridaH = heridaH + 10;
+                }
+                break;
+            case HOBBITS:
+                if(bestia.getTipo() == Bestia.Tipo.TRASGOS){
+                    heridaH = heridaH - 5;
+                }
+        }
+
+        System.out.println("Lucha entre " + heroe + " y " + bestia);
+
+        //Si el daño(herida)infringido es menor o igual a 0 nos dice que el ataque no se puede producir
+        if (heridaH <= 0) {
+            System.out.println("\tEl ataque no se puede producir porque la resistencia del defensor es mayor que la potencia de ataque");
+
+            //Si el ataque se produce porque no se cumple la condición anterior, nos da toda la información del ataque
+        } else {
+            bestia.quitarVida(heridaH);
+            System.out.println("\t" + heroe.getNombre() + " saca: " + ataqueH + " puntos de potencia de ataque y le quita: " + heridaH + " puntos de vida a " + bestia.getNombre());
+        }
+//Nos lanza un mensaje por consola si la bestia muere y nos da el nombre de la bestia que muere
+        if(bestia.estaMuerto()){
+            System.out.println("\t¡Muere " + bestia.getNombre() + "!");
+            bestias.remove(bestia);
+        }
+        //Si las listas de bestias o heroes no están vacías sigue el ataque
+        if(!bestias.isEmpty() && !heroes.isEmpty()) {
+            int ataqueB = bestia.lanzarDados();
+            int resistenciaH = heroe.getResistencia();
+            int heridaB = ataqueB - resistenciaH;
+
+            //Reduccion armadura ataque orcos
+            if (bestia.getTipo() == Bestia.Tipo.ORCOS){
+                heroe.reduceArmadura();
+            }
+
+            //Si el daño(herida)infringido es menor o igual a 0 nos dice que el ataque no se puede producir
+            if (heridaB <= 0) {
+                System.out.println("\tEl ataque no se puede producir porque la resistencia del defensor es mayor que la potencia de ataque");
+                System.out.println("\n");
+                //Si el ataque se produce porque no se cumple la condición anterior, nos da toda la información del ataque
+            } else {
+                heroe.quitarVida(heridaH);
+                System.out.println("\t" + bestia.getNombre() + " saca: " + ataqueB + " puntos de potencia de ataque y le quita: " + heridaB + " puntos de vida a " + heroe.getNombre());
+                System.out.println("\n");
+            }
+            //Nos lanza un mensaje por consola si el heroe muere y nos da el nombre del que muere
+            if (heroe.estaMuerto()) {
+                System.out.println("\t¡Muere " + heroe.getNombre() + "!");
+                heroes.remove(heroe);
+            }
+        }
+        //Si la lista de bestias se vacía, ganan los héroes
+        if(bestias.isEmpty()){
+            System.out.println("¡¡VICTORIA DE LOS HÉROES!!");
+            //Si la lista de héroes se vacía, ganan las bestias
+        } else if(heroes.isEmpty()) {
+            System.out.println("¡¡VICTORIA DE LAS BESTIAS!!");
+        }
+    }
+}
